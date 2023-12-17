2f91f33c84329294583ef587e22c8324b9f61d6e
Nikita Krylov <life.krylov@mail.ru>
2023-12-18 01:06:17 +0300

    add cert

--- taskapp/views/auth/helpers.py
+++ taskapp/views/auth/helpers.py
@@ -2,7 +2,8 @@ import hashlib
 import logging
 from datetime import datetime, timedelta
 
-from fastapi import Cookie, HTTPException, status
+from fastapi import Cookie, Header, HTTPException, status
+import re
 
 import taskapp.conf as conf
 from taskapp.repository.session import Session
@@ -18,13 +19,27 @@ def get_password_hash(password: str) -> str:
     return encrypted_password
 
 
-async def check_session(TGAGS: str | None = Cookie(default=None)) -> Session:
+async def check_session(
+    TGAGS: str | None = Cookie(default=None),
+    x_forwarded_for: str | None = Header(default=None),
+) -> Session:
     """
     Зависимость для получения сессии.
     """
     if not TGAGS:
         logger.info("No session cookie.")
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+
+    if not x_forwarded_for:
+        logger.info("x_forwarded_for is empty")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+
+    client_ip = x_forwarded_for.split(", ")[0]
+    if not re.search("10\\.0\\.0\\.\\d{1,3}", client_ip):
+        logger.info("%s is not trusted ip", client_ip)
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+    else:
+        logger.info("%s is trusted ip", client_ip)
 
     session = await app_state.session_repo.get(_id=TGAGS)
     if not session:
